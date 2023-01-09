56f90b225cb45fd05327b898d409a9254284a18d
Galina Fisher <galinafisher1010@gmail.com>
2023-01-09 21:23:22 +0100

    Add first commit

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,12 @@
 class NegativeValueException(Exception):
     """Исключение возникает при отсутствии переменных окружения."""
+
     ...
 
 
 class EndpointHTTPException(Exception):
     """Исключение возникает при недоступности эндпоинта."""
+
     ...
 
 
@@ -12,4 +14,5 @@ class InvalidTaskStatusException(Exception):
     """Исключение возникает при получении неожиданного статус домашней работы
     в ответе API.
     """
+
     ...
